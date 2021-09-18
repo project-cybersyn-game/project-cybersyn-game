f06f6827b95c112c5c81b04dae456e92e44be034
Hatman <88034713+Hutmensch@users.noreply.github.com>
2021-09-18 02:24:55 +0200

    Reduziertes/angepasstes Tutorial als Initialisierung
    Ich habe das Tutorial angepasst. Wir haben jetzt keine Schwerkraft mehr und eine angepasste Steuerung.
    
    Die Ordnerstruktur wurde über das phaser3-parcel-template erzeugt bzw. davon vorgeschlagen.
    
    Außerdem habe ich jetzt Typescript genutzt. (Deshalb hat das Ganze länger gedauert als gedacht, sorry.)
    
    Lokal getestet habe ich jetzt mithilfe von npm und Parcel.
    
    Verbessert mich bitte, falls davon irgendetwas falsch war.
    Und ich hoffe, das sieht alles wenigstens für den Anfang nicht zu komisch aus.
    Es wäre super, könntet ihr auch nochmal durch die Dateien und Ordner schauen, weil ich mir nicht sicher bin, ob wir davon überhaupt alles brauchen.

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import Phaser from 'phaser'
+
+import StandardScene from './scenes/HelloWorldScene'
+
+const config: Phaser.Types.Core.GameConfig = {
+	type: Phaser.AUTO,
+	width: 900,
+	height: 800,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 0 }
+		}
+	},
+	scene: [StandardScene]
+}
+
+export default new Phaser.Game(config)
---
+++ src/scenes/HelloWorldScene.ts
@@ -0,0 +1,116 @@
+import Phaser from 'phaser'
+
+
+enum ImageNames
+{
+    Roof = 'roof',
+    Ground = 'ground',
+    Dude = 'dude',
+    Sign = 'sign',
+}
+
+export default class HelloWorldScene extends Phaser.Scene
+{
+	
+    //Klassenvariablen festlegen
+    cursors
+    player
+    
+    constructor()
+	{
+		super('hello-world')
+	}
+
+	preload()
+    {
+        this.load.image(ImageNames.Roof, 'images/rooftiles.png')
+        this.load.image(ImageNames.Ground, 'http://labs.phaser.io/assets/textures/soil.png')
+        this.load.image(ImageNames.Sign, 'http://labs.phaser.io/assets/sets/objects/sign1.png')
+        this.load.spritesheet(
+            ImageNames.Dude, 
+            'http://labs.phaser.io/assets/sprites/dude.png', 
+            {
+                frameWidth: 32, 
+                frameHeight: 48
+            })
+    }
+
+    create()
+    {
+        this.add.image(400, 300, ImageNames.Ground).setScale(2)
+
+        //Objektgruppen erstellen
+        const signs = this.physics.add.staticGroup()
+        const houses = this.physics.add.staticGroup()
+        
+        //statische Objekte erstellen
+        signs.create(300, 400, ImageNames.Sign)
+
+        
+        //Spielfigur erstellen
+        this.player = this.physics.add.sprite(100, 450, ImageNames.Dude).setScale(2).refreshBody()
+        this.player.setCollideWorldBounds(true)
+        
+        //Animationen für Spielfigur
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers(ImageNames.Dude, {start: 0, end: 3}),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'front',
+            frames: [{ key: ImageNames.Dude, frame: 4}],
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers(ImageNames.Dude, {start: 5, end: 8}),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        //Collider für Spielfigur und Objekte
+        this.physics.add.collider(this.player, signs)
+        this.physics.add.collider(this.player, houses)
+
+        //Pfeiltasten "erstellen"
+        this.cursors = this.input.keyboard.createCursorKeys()
+
+        
+    }
+
+    update() 
+    {
+        
+        //Aktionen bei Pfeiltastendruck festlegen
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160)
+
+            this.player.anims.play('left', true)
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160)
+
+            this.player.anims.play('right', true)
+        }
+        else {
+            this.player.setVelocityX(0)
+
+            this.player.anims.play('front')
+        }
+        
+        if (this.cursors.up.isDown) {
+            this.player.setVelocityY(-160)
+        } else if (this.cursors.down.isDown) {
+            this.player.setVelocityY(160)
+        } else {
+            this.player.setVelocityY(0)
+        }
+
+    }
+
+    
+
+}
