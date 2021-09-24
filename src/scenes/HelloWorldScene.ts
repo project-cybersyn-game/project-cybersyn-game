2e7cfd7c6de7f3f1ae002f39917ab6c05eb5cb54
Hatman <88034713+Hutmensch@users.noreply.github.com>
2021-09-24 02:56:41 +0200

    Added use of tilemaps/tilesets and changed playermodel and animations

--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -3,11 +3,16 @@ import Phaser from 'phaser'
 
 enum ImageNames
 {
-    Roof = 'roof',
-    Ground = 'ground',
     Dude = 'dude',
-    Sign = 'sign',
-    TestTiles = 'testTiles'
+    TileA1 = 'tilea1',
+    TileA2 = 'tilea2',
+    TileA3 = 'tilea3',
+    TileA4 = 'tilea4',
+    TileA5 = 'tilea5',
+    TileB = 'tileb',
+    TileC = 'tilec',
+    TileD = 'tiled',
+    TileE = 'tilee'
 }
 
 export default class HelloWorldScene extends Phaser.Scene
@@ -16,6 +21,7 @@ export default class HelloWorldScene extends Phaser.Scene
     //Klassenvariablen festlegen
     cursors
     player
+    movementState = { 'type': 'idle', 'direction': 'down' }
     
     constructor()
 	{
@@ -24,70 +30,144 @@ export default class HelloWorldScene extends Phaser.Scene
 
 	preload()
     {
-        this.load.image(ImageNames.Roof, 'images/rooftiles.png')
-        this.load.image(ImageNames.Ground, 'http://labs.phaser.io/assets/textures/soil.png')
-        this.load.image(ImageNames.Sign, 'http://labs.phaser.io/assets/sets/objects/sign1.png')
+        // Tilemap-Bilder laden
+        this.load.image(ImageNames.TileA1, 'tilesets/tilea1.png')
+        this.load.image(ImageNames.TileA2, 'tilesets/tilea2.png')
+        this.load.image(ImageNames.TileA3, 'tilesets/tilea3.png')
+        this.load.image(ImageNames.TileA4, 'tilesets/tilea4.png')
+        this.load.image(ImageNames.TileA5, 'tilesets/tilea5.png')
+        this.load.image(ImageNames.TileB, 'tilesets/tileb.png')
+        this.load.image(ImageNames.TileC, 'tilesets/tilec.png')
+        this.load.image(ImageNames.TileD, 'tilesets/tiled.png')
+        this.load.image(ImageNames.TileE, 'tilesets/tilee.png')
+
+        // Tilemap-JSON laden
+        this.load.tilemapTiledJSON('map', 'tilemaps/TestTilemap.json')
+
+        // Spielfigur laden
         this.load.spritesheet(
             ImageNames.Dude, 
-            'http://labs.phaser.io/assets/sprites/dude.png', 
+            'character_sprites/char.png', 
             {
-                frameWidth: 32, 
-                frameHeight: 48
+                frameWidth: 25, 
+                frameHeight: 25
             })
-        this.load.image(ImageNames.TestTiles, "https://labs.phaser.io/assets/tilemaps/tiles/ground_1x1.png")
     }
 
     create()
     {
         
-        // Karte anhand der Tile-Indizes laden
-        const level = [
-            [ 0, 0, 0, 10, 6, 0, 0, 0, 0, 0, 0, 10, 6, 0, 0, 0, 0, 0, 0, 10, 6, 0, 0, 0 ],
-            [ 0, 0, 0, 12, 7, 2, 0, 0, 0, 0, 0, 12, 7, 2, 0, 0, 0, 0, 0, 12, 7, 2, 0, 0 ]
-        ]
-        const map = this.make.tilemap({ data: level, tileWidth: 32, tileHeight: 32})
-        const tiles = map.addTilesetImage(ImageNames.TestTiles)
-        const layer = map.createLayer(0, tiles, 0, 0)
-        
-        
-        //this.add.image(400, 300, ImageNames.Ground).setScale(2)
+        // Tilemap erstellen
+        const map = this.make.tilemap( {key: 'map'} )
 
-        //Objektgruppen erstellen
-        const signs = this.physics.add.staticGroup()
-        const houses = this.physics.add.staticGroup()
+        // Tileset-Bilder einem Array hinzufügen
+        var tileset = [map.addTilesetImage( 'tilea1', ImageNames.TileA1 )]
+        tileset.push(map.addTilesetImage( 'tilea2', ImageNames.TileA2 ))
+        tileset.push(map.addTilesetImage( 'tilea3', ImageNames.TileA3 ))
+        tileset.push(map.addTilesetImage( 'tilea4', ImageNames.TileA4 ))
+        tileset.push(map.addTilesetImage( 'tilea5', ImageNames.TileA5 ))
+        tileset.push(map.addTilesetImage( 'tileb', ImageNames.TileB ))
+        tileset.push(map.addTilesetImage( 'tilec', ImageNames.TileC ))
+        tileset.push(map.addTilesetImage( 'tiled', ImageNames.TileD ))
+        tileset.push(map.addTilesetImage( 'tilee', ImageNames.TileE ))
         
-        //statische Objekte erstellen
-        signs.create(300, 400, ImageNames.Sign)
+        
+
+        // Layer und Player in der richtigen Reihenfolge erstellen
+        const groundLayer = map.createLayer( '1_Ground', tileset )
+        const groundOverlayLayer = map.createLayer( '2_Ground_Overlay', tileset )
+        this.player = this.physics.add.sprite(100, 450, ImageNames.Dude).setScale(1.5).refreshBody()
+        const objectLayer = map.createLayer( '3_Objects', tileset)
+        const objectOverlayLayer = map.createLayer( '4_Objects_Overlay', tileset)
+        const objectOverlayOverlayLayer = map.createLayer( '5_Objects_Overlay_Overlay', tileset)
 
         
-        //Spielfigur erstellen
-        this.player = this.physics.add.sprite(100, 450, ImageNames.Dude)
+        // Kollisionseigenschaft spezieller Tiles entsprechender Ebenen setzen
+        // Kollision ist hierbei abhängig von der in der JSON festgelegten "collide"-Variable einzelner Tiles
+        objectLayer.setCollisionByProperty({ collides: true })
+
+        // Playerkollision setzen
         this.player.setCollideWorldBounds(true)
+        this.physics.add.collider(this.player, objectLayer)
         
-        //Animationen für Spielfigur
+        
+        // Animationen für Spielfigur
+        // walk animations
         this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers(ImageNames.Dude, {start: 0, end: 3}),
+            key: 'walk_left',
+            frames: this.anims.generateFrameNumbers(ImageNames.Dude, {
+                start: 40, 
+                end: 47
+            }),
             frameRate: 10,
             repeat: -1
         })
         this.anims.create({
-            key: 'front',
-            frames: [{ key: ImageNames.Dude, frame: 4}],
-            frameRate: 20,
-            repeat: -1
-        })
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers(ImageNames.Dude, {start: 5, end: 8}),
+            key: 'walk_right',
+            frames: this.anims.generateFrameNumbers(ImageNames.Dude, {
+                start: 32, 
+                end: 39
+            }),
             frameRate: 10,
             repeat: -1
         })
+        this.anims.create({
+            key: 'walk_up',
+            frames: this.anims.generateFrameNumbers(ImageNames.Dude, {
+                start: 24, 
+                end: 31
+            }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'walk_down',
+            frames: this.anims.generateFrameNumbers(ImageNames.Dude, {
+                start: 16, 
+                end: 23
+            }),
+            frameRate: 10,
+            repeat: -1
+        })
+        // idle animations
+        this.anims.create({
+            key: 'idle_left',
+            frames: this.anims.generateFrameNumbers(ImageNames.Dude, {
+                start: 14, 
+                end: 12
+            }),
+            frameRate: 5,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'idle_right',
+            frames: this.anims.generateFrameNumbers(ImageNames.Dude, {
+                start: 8, 
+                end: 11
+            }),
+            frameRate: 5,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'idle_up',
+            frames: this.anims.generateFrameNumbers(ImageNames.Dude, {
+                start: 4, 
+                end: 7
+            }),
+            frameRate: 5,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'idle_down',
+            frames: this.anims.generateFrameNumbers(ImageNames.Dude, {
+                start: 0, 
+                end: 3
+            }),
+            frameRate: 5,
+            repeat: -1
+        })
 
-        //Collider für Spielfigur und Objekte
-        this.physics.add.collider(this.player, signs)
-        this.physics.add.collider(this.player, houses)
-
+        
         //Pfeiltasten "erstellen"
         this.cursors = this.input.keyboard.createCursorKeys()
 
@@ -98,29 +178,37 @@ export default class HelloWorldScene extends Phaser.Scene
     {
         
         //Aktionen bei Pfeiltastendruck festlegen
+        // up/down
+        if (this.cursors.up.isDown) {
+            this.player.setVelocityY(-160)
+            this.movementState.type = 'walk'
+            this.movementState.direction = 'up'
+        } else if (this.cursors.down.isDown) {
+            this.player.setVelocityY(160)
+            this.movementState.type = 'walk'
+            this.movementState.direction = 'down'
+        } else {
+            this.player.setVelocityY(0)
+
+            this.movementState.type = 'idle'
+        }
+        // left/right
         if (this.cursors.left.isDown) {
             this.player.setVelocityX(-160)
-
-            this.player.anims.play('left', true)
+            this.movementState.type = 'walk'
+            this.movementState.direction = 'left'
         }
         else if (this.cursors.right.isDown) {
             this.player.setVelocityX(160)
-
-            this.player.anims.play('right', true)
-        }
-        else {
+            this.movementState.type = 'walk'
+            this.movementState.direction = 'right'
+        } else {
             this.player.setVelocityX(0)
-
-            this.player.anims.play('front')
         }
         
-        if (this.cursors.up.isDown) {
-            this.player.setVelocityY(-160)
-        } else if (this.cursors.down.isDown) {
-            this.player.setVelocityY(160)
-        } else {
-            this.player.setVelocityY(0)
-        }
+        
+        this.player.anims.play( this.movementState.type + '_' + this.movementState.direction, true )
+        
 
     }
 
