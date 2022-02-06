2f4fcf925104de0a5b8069d659ab5ea9526f32f7
JanaDeppe <janadeppe@gmail.com>
2022-02-06 15:40:36 +0100

    fixed glitches of uneven camera zoom

--- src/components/GameScene.ts
+++ src/components/GameScene.ts
@@ -215,6 +215,6 @@ export default abstract class GameScene extends Phaser.Scene {
     // add camera that follows the character
     this.cameras.main.setBounds(0, 0, boundLimitX, boundLimitY)
     this.cameras.main.startFollow(this.playerSprite, true)
-    this.cameras.main.setZoom(1.7)
+    this.cameras.main.setZoom(1.5)
   }
 }
